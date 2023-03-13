cb713c9334eeeb43ecbc3e8367e44e0bd1414c10
g-lamonov <owsrey@gmail.com>
2023-03-13 20:10:40 +0300

    refactor(interface): move interfaces to lib folder

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -1,3 +1,3 @@
-export * from './auth.interface';
-export * from './user.interface';
-export * from './course.interface';
+export * from './lib/auth.interface';
+export * from './lib/user.interface';
+export * from './lib/course.interface';
