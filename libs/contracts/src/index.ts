80062bc71ed616560f381f1908cd77876e889ed1
g-lamonov <owsrey@gmail.com>
2023-03-13 20:12:53 +0300

    feat(contract): payment contract

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/account/account.user-info';
 export * from './lib/account/account.user-courses';
 export * from './lib/account/account.change-profile';
 export * from './lib/course/course.get-course';
+export * from './lib/payment/payment.generate-link';
