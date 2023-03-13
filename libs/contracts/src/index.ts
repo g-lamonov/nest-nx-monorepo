589605ac321ddb31c242e3caae592f9aeada3c32
g-lamonov <owsrey@gmail.com>
2023-03-13 20:09:55 +0300

    feat(contract): get course contract

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/account/account.register';
 export * from './lib/account/account.user-info';
 export * from './lib/account/account.user-courses';
 export * from './lib/account/account.change-profile';
+export * from './lib/course/course.get-course';
