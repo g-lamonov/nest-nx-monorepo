f740df2a329f435532ef23aef88735e861cd41cd
g-lamonov <owsrey@gmail.com>
2023-03-13 13:02:21 +0300

    feat(controller): user commands and user queries controllers

--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/user.model';
 import { UserRepository } from './repositories/user.repository';
+import { UserCommands } from './user.commands';
+import { UserQueries } from './user.queries';
 
 @Module({
   imports: [
@@ -10,6 +12,7 @@ import { UserRepository } from './repositories/user.repository';
     ])
   ],
   providers: [UserRepository],
-  exports: [UserRepository]
+  exports: [UserRepository],
+  controllers: [UserCommands, UserQueries],
 })
 export class UserModule {}
