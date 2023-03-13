9b778ea1a787bed8ecdea0053969a79553d9762c
g-lamonov <owsrey@gmail.com>
2023-03-13 22:14:56 +0300

    feat(contract): buy course and check payment contracts

---
+++ libs/contracts/src/lib/account/account.check-payment.ts
@@ -0,0 +1,18 @@
+import { PurchaseState } from '@nest-monorepo/interfaces';
+import { IsString, } from 'class-validator';
+
+export namespace AccountCheckPayment {
+  export const topic = 'account.check-payment.command';
+
+  export class Request {
+    @IsString()
+    userId: string;
+
+    @IsString()
+    courseId: string;
+  }
+
+  export class Response {
+    status: PurchaseState;
+  }
+}
